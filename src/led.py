0150e30004c61c65af5d014e6dac25444ccf190d
James Dooley <81564302+dooley-ch@users.noreply.github.com>
2022-01-08 18:35:28 +0100

    Added code for LED

---
+++ src/led.py
@@ -0,0 +1,39 @@
+# ------------------------------------------------------------------------------------------
+# Copyright James A. Dooley 2021.
+#
+# Distributed under the MIT License.
+# (See accompanying file license.md file or copy at http://opensource.org/licenses/MIT)
+#
+# ------------------------------------------------------------------------------------------
+
+from microbit import pin0, sleep, button_b, display
+
+class Led:
+    def __init__(self, pin):
+        self._pin = pin
+        
+    def on(self):
+        self._pin.write_digital(1)
+    
+    def off(self):
+        self._pin.write_digital(0)
+
+def main():
+    led = Led(pin0)
+    led.off()
+
+    display.clear()
+    display.show('>')
+    
+    while True:
+        if button_b.was_pressed():
+            display.clear()
+            break
+        
+        led.on()
+        sleep(1000)
+        led.off()
+        sleep(1000)
+        
+if __name__ == '__main__':
+    main()
